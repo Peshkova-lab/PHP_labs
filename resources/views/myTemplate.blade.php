cf88f2d1d248a89c9f6d40bb1f8fa65d3884f5a0
Peshkova-lab-308 <kristino4ka928@gmail.com>
2021-09-05 15:04:53 +0300

    labs_1 Peshkova_308

---
+++ resources/views/myTemplate.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minium-scale=1.0">
+    <meta http-equiv="X-UA--Compatible" content="ie=edge">
+
+    <title>Individual task #1</title>
+</head>
+<body>
+<h1>It is individual task #1! </h1>
+<h1>Peshkova Kristina</h1>
+<h1>Group 308</h1>
+<h7>This layout is displayed when loading the main page (for this we changed the loading of the layout in the web.php file)</h7>
+</body>
+</html>
